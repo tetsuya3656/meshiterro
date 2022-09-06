ad2a8874cffe47222535d8def258989eda3b7de2
tetsuya <110829351+tetsuya3656@users.noreply.github.com>
2022-09-06 14:44:12 +0000

    [Create] Usersコントローラ作成、ユーザー詳細ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+ resources :users, only: [:show, :edit] 
  resources :post_images, only: [:new, :create, :index, :show, :destroy]
  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
