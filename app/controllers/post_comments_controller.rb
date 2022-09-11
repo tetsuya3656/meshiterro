009d7a86f9551f43d54220796a210a1e574223e8
tetsuya <110829351+tetsuya3656@users.noreply.github.com>
2022-09-11 08:08:46 +0000

    [Add] コメント削除機能追加

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,5 +1,5 @@
 class PostCommentsController < ApplicationController
-  
+
   def create
     post_image = PostImage.find(params[:post_image_id])
     comment = current_user.post_comments.new(post_comment_params)
@@ -7,11 +7,15 @@ class PostCommentsController < ApplicationController
     comment.save
     redirect_to post_image_path(post_image)
   end
-  
-  
+
+  def destroy
+    PostComment.find(params[:id]).destroy
+    redirect_to post_image_path(params[:post_image_id])
+  end
+
   private
   def post_comment_params
     params.require(:post_comment).permit(:comment)
-  end 
-  
+  end
+
 end
