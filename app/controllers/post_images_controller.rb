5e78b0edf3d23f60859477a27ca4b800dd25bfc4
tetsuya <110829351+tetsuya3656@users.noreply.github.com>
2022-09-11 07:52:54 +0000

    [Add] コメント投稿機能追加

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -8,7 +8,7 @@ class PostImagesController < ApplicationController
     @post_image.user_id = current_user.id
     @post_image.save
     redirect_to post_images_path
-  end  
+  end
 
 
   def index
@@ -17,17 +17,18 @@ class PostImagesController < ApplicationController
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
-  
+
   def destroy
     @post_image = PostImage.find(params[:id])
     @post_image.destroy
     redirect_to '/post_images'
-  end 
-  
+  end
+
   private
-  
+
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
-  end  
+  end
 end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,23 +1,24 @@
 class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
-  
-  
+  has_many :post_comments, dependent: :destroy
+
+
   def get_image
     if image.attached?
       image
     else
       'no_image.jpg'
-    end 
-  end 
-  
-  
+    end
+  end
+
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      iamge.attach(io:File.open(file_path),filename: 'default-image.jpg', content_type: 'image/jpeg') 
-    end 
+      iamge.attach(io:File.open(file_path),filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
     image
   end
-  
+
 end
